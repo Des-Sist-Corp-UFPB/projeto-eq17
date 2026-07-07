5fb510b5cba0c7847d7bd02bc6ff60ccd1317e0c
Ramon <ramon.alves@dcx.ufpb.br>
2026-07-07 15:48:31 -0300

    teste pipeline pos mudança runners

--- src/main/java/br/ufpb/dsc/republica/RepublicaApplication.java
+++ src/main/java/br/ufpb/dsc/republica/RepublicaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Classe principal da aplicação HomeHub — Gestão de Repúblicas Universitárias.
- * aa
+ * aaaa
  * <p>
  * A anotação {@code @SpringBootApplication} é um atalho que combina três
  * anotações:
